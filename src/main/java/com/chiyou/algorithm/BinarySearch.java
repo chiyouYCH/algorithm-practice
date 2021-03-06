ec492f950de06eaab7d06f858406f8cc0a9c901f
ChiYou <563318445@qq.com>
2021-03-06 20:06:01 +0800

    新增二分查找算法

---
+++ src/main/java/com/chiyou/algorithm/BinarySearch.java
@@ -0,0 +1,34 @@
+package com.chiyou.algorithm;
+
+import java.util.Arrays;
+
+/**
+ * 二分查找
+ */
+public class BinarySearch {
+
+    public static int search(int[] arr, int seachInt){
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+        int start = 0;
+        int end = arr.length-1;
+        int mid;
+        while (start <= end) {
+            mid = (end - start)/2 + start;
+            if (seachInt == arr[mid]) {
+                System.out.println("index :" + mid);
+                return mid;
+            } else if (seachInt > arr[mid]) {
+                start = mid+1;
+            } else {
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] source = {2,4,6,78,9,56,33,7};
+        BinarySearch.search(source,78);
+    }
+}
