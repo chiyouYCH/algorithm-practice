ce03b0e653e276da8ea78fd61d13fefc7aff6310
yangchenghao <563318445@qq.com>
2022-11-04 16:24:51 +0800

    20221103复习两两交换链表中的节点

--- src/main/java/com/chiyou/algorithm/linklist/swapPairs.java
+++ src/main/java/com/chiyou/algorithm/linklist/swapPairs.java
@@ -3,6 +3,7 @@ package com.chiyou.algorithm.linklist;
 import javax.swing.*;
 
 /**
+ * 两两交换链表中的节点
  * 题目；给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。
  * @author chiyou
  * @Description
@@ -77,18 +78,39 @@ public class swapPairs {
 
         return newNode;
     }
+    //20221103复习
+    //分析：1.结束条件，链表结束 2.返回什么，返回头节点 3.方法需要做什么：传入交换两个节点位置
+    public ListNode swapPairs1103(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode child = swapPairs1103(head.next.next);
+        ListNode newNode = head.next;
+        newNode.next = head;
+        newNode.next.next = child;
+
+        return newNode;
+    }
+
 
     public static void main(String[] args) {
         ListNode listNode = new ListNode();
         ListNode listNode1 = new ListNode();
         ListNode listNode3 = new ListNode();
+        ListNode listNode4 = new ListNode();
+        ListNode listNode5 = new ListNode();
         listNode.setVal(1);
         listNode1.setVal(2);
         listNode3.setVal(3);
+        listNode4.setVal(4);
+        listNode5.setVal(5);
+        listNode4.setNext(listNode5);
+        listNode3.setNext(listNode4);
         listNode1.setNext(listNode3);
         listNode.setNext(listNode1);
         swapPairs reverseList = new swapPairs();
         System.out.println(listNode.toString());
-        System.out.println(reverseList.swapPairs0719(listNode).toString());
+//        System.out.println(reverseList.swapPairs0719(listNode).toString());
+        System.out.println(reverseList.swapPairs1103(listNode).toString());
     }
 }
